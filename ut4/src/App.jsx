6cced6c1875cddd0b3672c0c8a6a50874953db76
MauroMachado <mauro.machadoc@correo.ucu.edu.uy>
2024-09-18 19:55:09 -0300

    ta2

--- ut4/src/App.jsx
+++ ut4/src/App.jsx
@@ -9,9 +9,29 @@ function App() {
   return (
     <>
       <div>
-        <Card title='Card 1' description='Description' assignedTo='Juancho' startDate='10/10/1999' endDate='11/9/2001'></Card>
-        <Card title='Card 2' description='Description' assignedTo='Juan' startDate='10/10/1989' endDate='11/9/2013'></Card>
-        <Card title='Card 2' description='Description' assignedTo='cho' startDate='10/10/1979' endDate='11/9/2010'></Card>
+        <Card>
+            <h2 className='card-title'> Card 1</h2>
+            <p className='card-description'>Description</p>
+            <p className='card-assignedTo'>Juancho</p>
+            <p className='card-startDate'>10/10/2000</p>
+            <p className='card-endDate'>9/9/1999</p>
+        </Card>
+
+        <Card>
+            <h2 className='card-title'> Card 2</h2>
+            <p className='card-description'>Description</p>
+            <p className='card-assignedTo'>Juancho</p>
+            <p className='card-startDate'>10/10/2000</p>
+            <p className='card-endDate'>9/9/1999</p>
+        </Card>
+
+        <Card>
+            <h2 className='card-title'> Card 3</h2>
+            <p className='card-description'>Description</p>
+            <p className='card-assignedTo'>Juancho</p>
+            <p className='card-startDate'>10/10/2000</p>
+            <p className='card-endDate'>9/9/1999</p>
+        </Card>
       </div>
     </>
   )
--- ut4/src/components/card/index.jsx
+++ ut4/src/components/card/index.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 //import './card.css'; // Assuming you have some CSS for styling
 import './style.css'
 
-const Card = ({ title, description, assignedTo, startDate, endDate }) => {
+const Card = ({children}) => {
     return (
         <div className="card">
-            <h2 className="card-title">{title}</h2>
-            <p className="card-description">{description}</p>
-            <p className="card-assignedTo">{assignedTo}</p>
-            <p className="card-startDate">{startDate}</p>
-            <p className="card-endDate">{endDate}</p>
+            {children}
         </div>
     );
 };
